465929b7924fe9011ea4369ac713979dc7c04930
Len <len.pgh@gmail.com>
2023-03-07 09:03:30 -0500

    changed to pin5 + 3.3v + ground

--- Signalwire_button_sms.ino
+++ Signalwire_button_sms.ino
@@ -13,88 +13,97 @@ const char * tooo = "17244445555"; //Replace with the number you send sms to
 const char * from = "14125551234"; //Replace with the number from your Signalwire space
 const char * sw_token = "PT123"; //Replace with your Signalwire space API Token
  
-// Define the button pin
-const int buttonPin = 4; //connect button to pin 4 and VCC
+const int buttonPin = 5; // the pin number for the button
+int buttonPushCounter = 0;  // counter for the number of button presses
+int buttonState = 0;        // current state of the button
+int lastButtonState = 0;    // previous state of the button
 
-bool sendSMS(const char * body){
- 
+bool sendSMS(const char* body) {
+
   std::stringstream url;
-  url << "https://" << space_url << api_path << account_sid <<"/Messages.json";
- 
+  url << "https://" << space_url << api_path << account_sid << "/Messages.json";
+
   std::stringstream urlEncodedBody;
   urlEncodedBody << "account_sid=" << account_sid << "&To=" << plus << "" << tooo << "&From=" << plus << "" << from << "&Body=" << body;
- 
 
   Serial.print("\nURL: ");
   Serial.println(url.str().c_str());
   Serial.print("Encoded body: ");
   Serial.println(urlEncodedBody.str().c_str());
-   
- 
+
   HTTPClient http;
- 
+
   http.begin(url.str().c_str());
   http.addHeader("Content-Type", "application/x-www-form-urlencoded");
   http.setAuthorization(account_sid, sw_token);
-    
-  int httpCode = http.POST(urlEncodedBody.str().c_str());                                               
-  
+
+  int httpCode = http.POST(urlEncodedBody.str().c_str());
+
   if (httpCode > 0) {
- 
-      String payload = http.getString();
- 
-      Serial.print("\nHTTP code: ");
-      Serial.println(httpCode);
- 
-      Serial.print("\nResponse: ");
-      Serial.println(payload);
-    }
- 
+
+    String payload = http.getString();
+
+    Serial.print("\nHTTP code: ");
+    Serial.println(httpCode);
+
+    Serial.print("\nResponse: ");
+    Serial.println(payload);
+  }
+
   else {
     Serial.println("Error on HTTP request:");
     Serial.println(httpCode);
   }
- 
+
   http.end();
- 
+
   return httpCode == 201;
- 
+
 }
 
 void setup() {
-  
+
+  // initialize the button pin as a input:
+  pinMode(buttonPin, INPUT);
+  // initialize the LED as an output:
+//  pinMode(ledPin, OUTPUT);
   Serial.begin(115200);
- 
-  WiFi.begin(ssid, password); 
-  
+
+  WiFi.begin(ssid, password);
+
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi..");
   }
-  
+
   Serial.println("Connected to the WiFi network");
 
-  // Set the button pin as an input
-  pinMode(buttonPin, INPUT);
- 
 }
-  
- 
+
 void loop() {
+
   // Read the state of the button
-  int buttonState = digitalRead(buttonPin);
+  buttonState = digitalRead(buttonPin);
 
   // If the button is pressed, send an SMS
-  if (buttonState == HIGH) {
-    bool result = sendSMS("Button pushed on ESP32. Thank you for choosing https://signalwire.com");
- 
-    if(result){
-      Serial.println("\nSMS sent");
-    }else{
-      Serial.println("\nSMS failed");
-    }
+    if (buttonState == lastButtonState) {
+      
+      if (buttonState == HIGH) {
+      // if the current state is HIGH then the button went from off to on:
+      buttonPushCounter++;
+      Serial.println("on");
+      Serial.print("number of button pushes: ");
+      Serial.println(buttonPushCounter);
+        bool result = sendSMS("Button pushed on ESP32. Thank you for choosing https://signalwire.com");
+      delay(1000);
+    } else {
+      // if the current state is LOW then the button went from on to off:
+     // Serial.println("off");
 
-    // Add a delay to debounce the button
-    delay(1000);
-  }
+        delay(500);
+      }  } 
+
+  lastButtonState = buttonState; // store the current state of the button
+
+
 }
